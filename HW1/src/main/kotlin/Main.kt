2db850a1d6de5d663d31e2ae29b2833bd2e37875
AntonHolden <robsmt@mail.ru>
2023-03-07 21:31:50 +0300

    Moved Prime to separate module

--- HW1/src/main/kotlin/Main.kt
+++ HW1/src/main/kotlin/Main.kt
@@ -1,17 +1,1 @@
-import kotlin.math.*
-
-object Prime {
-    private val isPrimeMap = HashMap<Long, Boolean>()
-    private fun Long.hasNoNontrivialDivisors() = (2L..sqrt(this.toDouble()).toLong())
-        .all { this % it != 0L }
-
-    fun sequence(s: Sequence<Long>): Sequence<Long> = s
-        .filter {
-            isPrimeMap.getOrPut(it) {
-                (it.hasNoNontrivialDivisors())
-                        && (it > 1)
-            }
-        }
-}
-
 fun main() {}
---
+++ HW1/src/main/kotlin/Prime.kt
@@ -0,0 +1,15 @@
+import kotlin.math.sqrt
+
+object Prime {
+    private val isPrimeMap = HashMap<Long, Boolean>()
+    private fun Long.hasNoNontrivialDivisors() = (2L..sqrt(this.toDouble()).toLong())
+        .all { this % it != 0L }
+
+    fun sequence(s: Sequence<Long>): Sequence<Long> = s
+        .filter {
+            isPrimeMap.getOrPut(it) {
+                (it.hasNoNontrivialDivisors())
+                        && (it > 1)
+            }
+        }
+}
